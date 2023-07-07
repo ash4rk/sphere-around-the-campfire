3821cfd1a242250659d7ab31a8bf18e04bd81064
ash4rk <cool.shark.98@mail.ru>
2023-07-07 17:20:36 +0300

    REFACTOR: Generalise exit_dialog into custom_dialog class

--- scenes/custom_dialog.gd
+++ scenes/custom_dialog.gd
@@ -1,7 +1,11 @@
 extends ColorRect
 
+class_name CustomDialog
+
+signal yes_button_pressed
+
 func _on_yes_button_pressed():
-	get_tree().quit()
+	emit_signal("yes_button_pressed")
 
 func _on_no_button_pressed():
 	self.queue_free()
--- scenes/exit_button.gd
+++ scenes/exit_button.gd
@@ -1,7 +1,11 @@
 extends Button
 
-@onready var EXIT_DIALOG = preload("res://scenes/exit_dialog.tscn")
+@onready var DIALOG_SCENE = preload("res://scenes/custom_dialog.tscn")
 
 func _on_pressed():
-	var exit_instance = EXIT_DIALOG.instantiate()
+	var exit_instance: CustomDialog = DIALOG_SCENE.instantiate()
 	get_tree().root.add_child(exit_instance)
+	exit_instance.yes_button_pressed.connect(quit)
+	
+func quit():
+	get_tree().quit()
