db113b7234c9accf3282365c7b4e499e77dd5cd0
ash4rk <cool.shark.98@mail.ru>
2023-07-08 10:27:04 +0300

    FEAT: Rotation speed and radius values display on ui

--- scenes/level_ui.gd
+++ scenes/level_ui.gd
@@ -1,9 +1,24 @@
 extends Control
 
-@export var sphere: CSGSphere3D
+@export var sphere: RotationSphere
+@onready var radius_value_label: Label = $VBoxContainer/RadiusContainer/RadiusValueLabel
+@onready var speed_value_label: Label = $VBoxContainer/SpeedContainer/SpeedValueLabel
+
+func _ready():
+	speed_value_label.text = str(sphere.rotation_speed)
+	radius_value_label.text = str(sphere.rotation_radius)
+	
+	sphere.rotation_radius_changed.connect(_on_rotation_radius_changed)
+	sphere.rotation_speed_changed.connect(_on_rotation_speed_changed)
 
 func _on_speed_h_slider_value_changed(value):
 	sphere.rotation_speed = value
 
 func _on_radius_h_slider_value_changed(value):
 	sphere.rotation_radius = value
+
+func _on_rotation_radius_changed(new_value):
+	radius_value_label.text = str(new_value)
+
+func _on_rotation_speed_changed(new_value):
+	speed_value_label.text = str(new_value)
--- scenes/sphere.gd
+++ scenes/sphere.gd
@@ -1,8 +1,13 @@
 extends Node3D
 
+class_name RotationSphere
+
+signal rotation_radius_changed (new_value)
+signal rotation_speed_changed (new_value)
+
 @export var target_node: Node3D
-@export var rotation_radius: float = 5.0
-@export var rotation_speed: float = 1.0
+@export var rotation_radius: float = 5.0 : set = _set_rotation_radius
+@export var rotation_speed: float = 1.0 : set = _set_rotation_speed
 
 var angle: float = 0.0
 
@@ -12,3 +17,11 @@ func _process(delta: float):
 	var x = target_node.position.x + rotation_radius * cos(angle)
 	var z = target_node.position.z + rotation_radius * sin(angle)
 	position = Vector3(x, position.y, z)
+
+func _set_rotation_radius(new_value):
+	rotation_radius = new_value
+	rotation_radius_changed.emit(rotation_radius)
+	
+func _set_rotation_speed(new_value):
+	rotation_speed = new_value
+	rotation_speed_changed.emit(rotation_speed)
