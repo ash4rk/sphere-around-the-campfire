4de8cb80ff88c44bb4d170f622fff75530cf39f5
ash4rk <cool.shark.98@mail.ru>
2023-07-09 17:59:04 +0300

    FEAT: Implement ripple effect on ui_effect_button

--- scenes/menu/ui_effect_button.gd
+++ scenes/menu/ui_effect_button.gd
@@ -1,13 +1,33 @@
 extends Button
 
 var _SPARKLES_SCENE: PackedScene = preload("res://scenes/ui-effects/sparkles.tscn")
-@export var glitch_effect: ColorRect
+@onready var _ripple_effect: ColorRect = $RippleEffect
+@export var _glitch_effect: ColorRect
 
 func _on_pressed():
-	glitch_effect._emit_glitch_effect()
+	_glitch_effect._emit_glitch_effect()
 	_emit_sparkles_effect()
+	_emit_ripple_effect()
 
 func _emit_sparkles_effect():
 	var sparkles_instance = _SPARKLES_SCENE.instantiate()
 	get_parent().add_child(sparkles_instance)
 	sparkles_instance.global_position = get_global_mouse_position()
+
+func _emit_ripple_effect():
+	var tween = get_tree().create_tween();
+	tween.tween_method(_set_shader_value, -0.1, 2.0, 0.5)
+	_ripple_effect.material.set_shader_parameter("center", _get_normalized_mouse_position())
+
+func _set_shader_value(value: float):
+	_ripple_effect.material.set_shader_parameter("distortion_force", value)
+	_ripple_effect.material.set_shader_parameter("size", value)
+	
+func _get_normalized_mouse_position() -> Vector2:
+	var buttonRect = self.get_rect()
+	var mousePos = self.get_local_mouse_position()
+
+	var xNormalized = mousePos.x / buttonRect.size.x
+	var yNormalized = mousePos.y / buttonRect.size.y
+
+	return Vector2(xNormalized, yNormalized)
